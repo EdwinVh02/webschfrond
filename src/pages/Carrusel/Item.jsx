989fad19b949fb4c1bb3a4950b0f9a641a292bc9
Caih2023 <estadiacaih@gmail.com>
2024-02-09 12:40:54 -0600

    carrusel

--- src/pages/Carrusel/Item.jsx
+++ src/pages/Carrusel/Item.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Paper } from '@mui/material';
+import React from "react";
+import { Paper, Box, Typography } from "@mui/material";
 
-function Item({item})
-{
-    return (
-        <Paper>          
-            <img src={item.imagen} alt={item.titulo} style={{width: '100%', height: '50vh'}}/>
-            <div className="desciption" style={{display: 'flex', justifyContent:'center'}}>
-                <h2>{item.titulo}</h2>
-            </div>
-        </Paper>
-    )
+function Item({ item }) {
+  return (
+    <Paper elevation={3}>
+      <Box p={2}>
+        <img
+          src={item.imagen}
+          alt={item.titulo}
+          style={{ width: "100%", height: "50vh", objectFit: "cover" }}
+        />
+      </Box>
+      <Box p={2} textAlign="center">
+        <Typography variant="h6" gutterBottom>
+          {item.titulo}
+        </Typography>
+      </Box>
+    </Paper>
+  );
 }
 
 export default Item;
