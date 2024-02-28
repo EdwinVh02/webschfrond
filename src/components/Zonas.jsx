cdba42bd7d1c3cf3b1f6b3cb8306418bb190dd94
EdwinVh02 <skrillexelmejor77@gmail.com>
2024-02-28 12:46:37 -0600

    scroll

--- src/components/Zonas.jsx
+++ src/components/Zonas.jsx
@@ -2,28 +2,24 @@ import React from "react";
 import { motion } from "framer-motion";
 import {
   styled,
-  Button,
   CardActionArea,
-  CardActions,
-  Typography,
   CardMedia,
   CardContent,
   Card,
   Grid,
   Paper,
   Box,
+  Typography,
 } from "@mui/material";
+import habitaciones from "./habitaciones.json";
 
 const Titulo = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
   textAlign: "center",
-  backgroundColor:"#DBB689"
+  backgroundColor: "#DBB689",
 }));
 
 const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(2),
-  backgroundColor:"#DBB689",
-  textAlign: "center"
+  textAlign: "center",
 }));
 
 const Zonas = () => {
@@ -31,7 +27,7 @@ const Zonas = () => {
     <motion.div
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
-      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}//Duracion de transicion de las interfaces 
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
     >
       <Box sx={{ flexGrow: 1 }} >
         <Grid item xs={15} sx={{ mb: 10 }}>
@@ -39,219 +35,122 @@ const Zonas = () => {
             <h3>Habitaciones Disponibles</h3>
           </Titulo>
         </Grid>
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            <Item elevation={0}>
-              <Grid container spacing={5}>
-                <Grid item xs={4}>
-                  <Item elevation={0} marginLeft={5} >
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_juniorsuite.jpg")}
-                          alt="Habitacion Estandar"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                           Habitacion Estandar 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">                            
-                            <strong>CUENTA CON:</strong><br/>
-                              1 Cama Matrimonial <br/>
-                            <strong>SERVICIOS:</strong><br/>
-                              Wifi,
-                              Tv por cable, 
-                              Agua fría y caliente, 
-                              Aire acondicionado,
-                              Servicios<br/>
-                            <strong>AMENIDADES:</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal<br />
-                              <strong>Precio: $679.00</strong><br/>
-                            <strong>1 Persona</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item elevation={0} marginLeft={5} >
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_estandar2Camas.jpg")}
-                          alt="Habitacion Estandar"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                           Habitacion Estandar 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">                            
-                            <strong>CUENTA CON:</strong><br/>
-                              2 Camas Matrimoniales<br/>
-                            <strong>SERVICIOS:</strong><br/>
-                              Wifi,
-                              Tv por cable, 
-                              Agua fría y caliente, 
-                              Aire acondicionado,
-                              Servicios<br/>
-                            <strong>AMENIDADES:</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal<br />
-                            <strong>Precio: $739</strong><br/>
-                            <strong>2 Personas</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item elevation={0} marginLeft={5} >
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_estandar2Camas.jpg")}
-                          alt="Habitacion Estandar"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                           Habitacion Estandar 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">                            
-                            <strong>CUENTA CON:</strong><br/>
-                              3 Camas Individuales<br/>
-                            <strong>SERVICIOS:</strong><br/>
-                              Wifi,
-                              Tv por cable, 
-                              Agua fría y caliente, 
-                              Aire acondicionado,
-                              Servicios<br/>
-                            <strong>AMENIDADES:</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal<br />
-                            <strong>Precio: $819</strong><br/>
-                            <strong>3 Personas</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item elevation={0}>
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_juniorsuite.jpg")}
-                          alt="Habitacion Junior Suite"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                            Habitacion Junior Suite 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            <strong>CUENTA CON:</strong><br />
-                              1 Cama king size,
-                              Mesa y silla de trabajo,
-                              Sofá,
-                              Frigobar pequeño.<br/>
-                              <strong>SERVICIOS:</strong><br />
-                              Tv por cable,
-                              Agua fría y calientes, 
-                              Aire acondicionado<br/>
-                            <strong>AMENIDADES</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal.<br />
-                              <strong>Precio: $819</strong><br/>
-                              <strong>1 Persona</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item elevation={0}>
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_juniorsuite.jpg")}
-                          alt="Habitacion Junior Suite"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                            Habitacion Junior Suite 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            <strong>CUENTA CON:</strong><br />
-                              2 Camas Individuales king size,
-                              Mesa y silla de trabajo,
-                              Sofá,
-                              Frigobar pequeño.<br/>
-                              <strong>SERVICIOS:</strong><br />
-                              Tv por cable,
-                              Agua fría y calientes, 
-                              Aire acondicionado<br/>
-                            <strong>AMENIDADES</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal.<br />
-                              <strong>Precio: $819</strong><br/>
-                              <strong>2 Personas</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item elevation={0}>
-                    <Card sx={{ maxWidth: 345 }}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="350"
-                          image={require("../assets/habitacion_juniorsuite.jpg")}
-                          alt="Habitacion Junior Suite"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                            Habitacion Junior Suite 
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            <strong>CUENTA CON:</strong><br />
-                              3 Camas Individuales king size,
-                              Mesa y silla de trabajo,
-                              Sofá,
-                              Frigobar pequeño.<br/>
-                              <strong>SERVICIOS:</strong><br />
-                              Tv por cable,
-                              Agua fría y calientes, 
-                              Aire acondicionado<br/>
-                            <strong>AMENIDADES</strong><br/>
-                              Desayuno continental, 
-                              Kit de aseo personal.<br />
-                              <strong>Precio: $819</strong><br/>
-                              <strong>3 Personas</strong>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Item>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <div style={{ height: '900px', overflowY: 'scroll' }}>
+              {habitaciones.map((habitacion, index) => (
+                <React.Fragment key={index}>
+                  <Grid item xs={12} sm={6} md={6} key={habitacion.id}>
+                    <Item elevation={0}>
+                      <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            height="350"
+                            image={require(`../assets/${habitacion.imagen}`)}
+                            alt={habitacion.titulo}
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="div"
+                            >
+                              {habitacion.titulo}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="text.secondary"
+                            >
+                              <strong>CUENTA CON:</strong>
+                              <br />
+                              {habitacion.camas} Cama
+                              {habitacion.camas > 1 ? "s" : ""} Matrimonial
+                              {habitacion.camas > 1 ? "es" : ""}
+                              <br />
+                              <strong>SERVICIOS:</strong>
+                              <br />
+                              {habitacion.servicios.join(", ")}
+                              <br />
+                              <strong>AMENIDADES:</strong>
+                              <br />
+                              {habitacion.amenidades.join(", ")}
+                              <br />
+                              <strong>
+                                Precio: ${habitacion.precio.toFixed(2)}
+                              </strong>
+                              <br />
+                              <strong>
+                                {habitacion.capacidad} Persona
+                                {habitacion.capacidad > 1 ? "s" : ""}
+                              </strong>
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Item>
+                  </Grid>
+                </React.Fragment>
+              ))}
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div style={{ height: '900px', overflowY: 'scroll' }}>
+              {habitaciones.map((habitacion, index) => (
+                <React.Fragment key={index}>
+                  <Grid item xs={12} sm={6} md={6} key={habitacion.id}>
+                    <Item elevation={0}>
+                      <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            height="350"
+                            image={require(`../assets/${habitacion.imagen}`)}
+                            alt={habitacion.titulo}
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="div"
+                            >
+                              {habitacion.titulo}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="text.secondary"
+                            >
+                              <strong>CUENTA CON:</strong>
+                              <br />
+                              {habitacion.camas} Cama
+                              {habitacion.camas > 1 ? "s" : ""} Matrimonial
+                              {habitacion.camas > 1 ? "es" : ""}
+                              <br />
+                              <strong>SERVICIOS:</strong>
+                              <br />
+                              {habitacion.servicios.join(", ")}
+                              <br />
+                              <strong>AMENIDADES:</strong>
+                              <br />
+                              {habitacion.amenidades.join(", ")}
+                              <br />
+                              <strong>
+                                Precio: ${habitacion.precio.toFixed(2)}
+                              </strong>
+                              <br />
+                              <strong>
+                                {habitacion.capacidad} Persona
+                                {habitacion.capacidad > 1 ? "s" : ""}
+                              </strong>
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Item>
+                  </Grid>
+                </React.Fragment>
+              ))}
+            </div>
           </Grid>
         </Grid>
       </Box>
